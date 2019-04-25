ef75ba89e94c429349f87745dea70722cc713779
tlinkner <todd@toddlinkner.com>
2019-04-25 19:30:51 -0400

    try github pages from build

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -3,12 +3,16 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
 	plugins:[
 		new webpack.DefinePlugin({
 			'process.env': {'NODE_ENV':JSON.stringify('production')}
-		})
+		}),
+    new CopyPlugin([
+      { from: 'dist', to: "../docs" }
+    ])
 	],
 	optimization: {
 		minimizer: [new UglifyJSPlugin()]
